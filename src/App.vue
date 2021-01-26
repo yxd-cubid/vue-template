c9e086ec3682090926093c64c46729d3f10e608b
cubid <yxd.cubid@gmail.com>
2021-01-26 21:09:17 +0800

    初始化vue-temp模板项目

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld/>
+</template>
+
+<script>
+
+// import Vue from 'vue'
+const HelloWorld = {
+  data: function() {
+    return {
+      message: 'Hello World'
+    }
+  },
+  methods: {
+    test() {
+      console.log("test方法执行");
+    }
+  },
+  mounted() {
+    console.log("生命周期函数执行");
+  },
+  render: function() {
+    return (
+      <div>
+        <h2>Hello World Test {this.message}</h2>
+        <button onClick={() => this.test()}>按钮</button>
+      </div>
+      )
+  }
+}
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
